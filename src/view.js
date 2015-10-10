c3b29a4d9da7ab1cd0fb08ae079bbf0dafa813e9
bouzuya <m@bouzuya.net>
2015-10-10 20:38:01 +0900

    extract views/render

--- src/view.js
+++ src/view.js
@@ -1,37 +1,9 @@
-import {h} from '@cycle/dom';
+import render from './views/render';
 
-function renderHeightSlider(height) {
-  return h('div', [
-    h('labeled-slider#height', {
-      key: 1, label: 'Height', unit: 'cm',
-      min: 140, max: 210, initial: height
-    })
-  ]);
-}
-
-function renderWeightSlider(weight) {
-  return h('div', [
-    'Weight ' + weight + 'kg',
-    h('input#weight', { type: 'range', min: 40, max: 140, value: weight })
-  ]);
-}
-
-function renderToggleCheckBox(toggled) {
-  return h('div', [
-    h('input', { type: 'checkbox' }),
-    'Toggle me',
-    h('p', toggled ? 'ON': 'off')
-  ]);
-}
-
-export default function(stateStream) {
-  return {
-    DOM: stateStream.map(({ height, weight, toggled }) =>
-      h('div', [
-        renderHeightSlider(height),
-        renderWeightSlider(weight),
-        renderToggleCheckBox(toggled)
-      ])
-    )
+export default function(state$) {
+  const vtree$ = render(state$);
+  const requests = {
+    DOM: vtree$
   };
+  return requests;
 }
---
+++ src/views/render.js
@@ -0,0 +1,35 @@
+import {h} from '@cycle/dom';
+
+function renderHeightSlider(height) {
+  return h('div', [
+    h('labeled-slider#height', {
+      key: 1, label: 'Height', unit: 'cm',
+      min: 140, max: 210, initial: height
+    })
+  ]);
+}
+
+function renderWeightSlider(weight) {
+  return h('div', [
+    'Weight ' + weight + 'kg',
+    h('input#weight', { type: 'range', min: 40, max: 140, value: weight })
+  ]);
+}
+
+function renderToggleCheckBox(toggled) {
+  return h('div', [
+    h('input', { type: 'checkbox' }),
+    'Toggle me',
+    h('p', toggled ? 'ON': 'off')
+  ]);
+}
+
+export default function(state$) {
+  return state$.map(({ height, weight, toggled }) =>
+    h('div', [
+      renderHeightSlider(height),
+      renderWeightSlider(weight),
+      renderToggleCheckBox(toggled)
+    ])
+  );
+}
