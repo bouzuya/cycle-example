2f59e65abf583b5622cac47780be285758b7a272
bouzuya <m@bouzuya.net>
2015-10-10 23:16:45 +0900

    fix bug

--- src/intents/change-posts.js
+++ src/intents/change-posts.js
@@ -1,4 +1,5 @@
 export default function({ HTTP }) {
+  const url = 'http://blog.bouzuya.net/posts.json';
   const changePosts = HTTP
     .filter((response$) => response$.request.url === url)
     .mergeAll()
